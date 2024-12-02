db0ea6bb504074b982c38f06da15a8c556934cdc
JoelBright <joelbright@live.com>
2024-12-02 23:45:06 +0530

    Completed and tested Part 2 also working correctly.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,13 +14,12 @@ public class Main {
 
     private static final String Path = "src/main/resources/";
 
-    static int calculateTotalDistance(String fileName){
-        String filePath = Path + fileName;
-        int requiredTotal = 0;
+    static Result readAndSortListsIn(String fileName) {
 
         List<Integer> firstColumn = new ArrayList<>();
         List<Integer> secondColumn = new ArrayList<>();
 
+        String filePath = Path + fileName;
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
@@ -39,17 +38,25 @@ public class Main {
         System.out.println("First Column: " + firstColumn);
         System.out.println("Second Column: " + secondColumn);
 
+        return new Result(firstColumn, secondColumn);
+    }
+
+    static int calculateTotalDistance(List<Integer> list1, List<Integer> list2) {
+        int requiredTotal = 0;
+
         int index = 0;
-        for(int x: firstColumn){
-            int y = secondColumn.get(index);
+        for (int x : list1) {
+            int y = list2.get(index);
 
-            int distance = x-y;
-            System.out.println("x:"+x+", y:"+y+" thus, distance at index "+index+" is ="+distance);
+            int distance = x - y;
+            System.out.println("x:" + x + ", y:" + y + " thus, distance at index " + index + " is =" + distance);
 
-            if (distance < 0) { distance *= -1; }
+            if (distance < 0) {
+                distance *= -1;
+            }
             requiredTotal += distance;
 
-            System.out.println("updated requiredTotal is ="+requiredTotal);
+            System.out.println("updated requiredTotal is =" + requiredTotal);
 
             index++;
         }
@@ -57,26 +64,42 @@ public class Main {
         return requiredTotal;
     }
 
+    static long calculateSimilarityScore(List<Integer> list1, List<Integer> list2) {
+        long requiredScore = 0;
+
+        for (int x : list1) {
+
+            long countOfX = list2.stream().filter(e -> e.equals(x)).count();
+
+            requiredScore += countOfX * x;
+            if (countOfX > 0)
+                System.out.println("x:" + x + ", countOfX:" + countOfX + " thus, requiredScore is now =" + requiredScore);
+        }
+
+        return requiredScore;
+    }
+
     public static void main(String[] args) {
 
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hi, select the file to run:\n 1-ExampleIn.txt\n 2-Input.txt\n 3-TestIn.txt\n");
+        System.out.println("Hi, select the file to run:\n 1-ExampleIn.txt\n 2-Input.txt\n 3-TestIn.txt");
         Scanner scanner = new Scanner(System.in);
         String selection = scanner.nextLine();
 
-        String fileName = "ExampleIn.txt";
+        String fileName = switch (selection) {
+            case "2" -> "Input.txt";
+            case "3" -> "TestIn.txt";
+            default -> "ExampleIn.txt";
+        };
 
-        switch (selection){
-            case "2" :  fileName = "Input.txt";
-                break;
-            case "3" :  fileName = "TestIn.txt";
-                break;
-            default:    fileName = "ExampleIn.txt";
-        }
+        Result inputValues = readAndSortListsIn(fileName);
 
-        System.out.print("The required total distance is: "+ calculateTotalDistance(fileName));
+        System.out.println("The required total distance answer for part 1: " + calculateTotalDistance(inputValues.value1(), inputValues.value2()));
 
+        System.out.println("The required similarity score answer for part 2: " + calculateSimilarityScore(inputValues.value1(), inputValues.value2()));
+    }
 
+    public record Result(List<Integer> value1, List<Integer> value2) {
     }
 }
